d1346f8d2cbf6e7f10d73c8897d9ee3017248ecd
Kealoha1988 <kealohaabe1988@gmail.com>
2021-01-26 13:41:12 -0500

    commit tree 4a970e0a65b6c56a5b09e8414635beb3453970e7

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,2 +1,15 @@
 module ArtistsHelper
+
+
+def display_artist
+  if artist.song.find_by(artist_id)
+    link_to @artist
+  else
+    link_to "Add Artist", song_edit_path(@song)
+  end
 end
+
+end
+
+If an artist is already associated with the song, return a link to the artist's show page
+If an artist is not associated with the song (a.k.a. 'else'), return a link to the song's edit page, with a link text of "Add Artist"
